745eb1214580ecef0e163368defb05106d6137ef
hmiyamoto2000 <135317049+hmiyamoto2000@users.noreply.github.com>
2025-01-28 12:43:32 +0900

    Update MLs.R

--- MLs.R
+++ MLs.R
@@ -100,7 +100,7 @@ for(i in 1:length(pred)){
   else if(pred[i]==4) {pred[i]="Control"}
   else if(pred[i]==5) {pred[i]="Test"}
   else if(pred[i]==6) {pred[i]="Control"}
-  else if(pred[i]==7) {pred[i]="Test"}
+  else if(pred[i]==7) {pred[i]="Test"} #Pig_Comp_ThB
 }
 
 table(x_1_f,pred) # Please check 100% accuracy rate
