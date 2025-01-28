9dbe746310dc68c4b388350779649f732777eb3c
hmiyamoto2000 <135317049+hmiyamoto2000@users.noreply.github.com>
2025-01-28 12:54:17 +0900

    Update MLs_9groups_for_RF_and_XGBoost.R

--- MLs_9groups_for_RF_and_XGBoost.R
+++ MLs_9groups_for_RF_and_XGBoost.R
@@ -90,7 +90,7 @@ nround <- 27
 bst <- xgboost(param=param, data = x[trind,], label = y, nrounds=nround)
 pred <- predict(bst,x[teind,])
 
-#9 groups
+#9 groups (for Group_9 in Fig.S11)
 x_1_f <- FCPC.test[,1]
 for(i in 1:length(pred)){
   if(pred[i]==0) {pred[i]="Control"}
